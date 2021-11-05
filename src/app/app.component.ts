832c289596e11cc1fb3eb3c43ddeb9e8ac48ffa1
regnom <regnomtv@gmail.com>
2021-11-04 21:15:23 -0700

    Adding all that I've worked on today yo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'foundryfuckery';
+
+  /** Swapping User Information */
+  public user1 = "Chazland";
+
+  /** Current Charater Selected information - Set for chaz till automation */
+  public currentName ="Chazland";
+  public currentRace = "Human";
+  public currentAlign = "Neutral";
+  public currentBG = "Noble";
+
+  /** Attributes */
+  public currentStr = 15;
+  public currentStrMod = (this.currentStr - 10)/2; 
+
+  public currentDex = 15;
+  public currentDexMod = (this.currentDex - 10)/2; 
+
 }
